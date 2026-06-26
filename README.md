adbb398fb26c05500e018493cd9caed4e68587f8
santi <santiagoespitic@gmail.com>
2026-06-25 19:31:26 -0500

    feat: implement profile modal editing and form submission logic

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Descripción
 
-El proyecto define una estructura de datos estática (`initialCards`) que contiene información sobre diferentes destinos naturales (nombre y enlace de imagen). Actualmente, incluye un script que recorre esta colección para procesar y listar los nombres en la consola.
+El proyecto define una estructura de datos estática (`initialCards`) que contiene información sobre diferentes destinos naturales (nombre y enlace de imagen), incluye un script que recorre esta colección para procesar y listar los nombres en la consola y adicionalmente, implementa de forma robusta la lógica para la manipulación del Document Object Model (DOM).
 
 ## Estructura del Código
 
@@ -10,3 +10,9 @@ El script principal consta de dos partes clave:
 
 1. **Arreglo de Datos**: Un array de objetos (`const initialCards`) con propiedades `name` y `link`.
 2. **Método de Recorrido**: El uso de `.forEach()` para iterar de manera eficiente sobre cada elemento.
+3. **Funciones de Responsabilidad Única**:
+   - `openModal()` y `closeModal()`
+   - `fillProfileForm()`
+   - `handleOpenEditModal()`
+   - `handleProfileFormSubmit()`
+4. **Escuchadores de Eventos**: Vinculación de los eventos `click` y `submit` a sus respectivos botones y formularios al final del script.
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-// Lista de tarjetas
+// DATOS INICIALES
+
 const initialCards = [
   {
     name: "Valle de Yosemite",
@@ -26,7 +27,50 @@ const initialCards = [
   },
 ];
 
-// Recorre el array y registra el nombre del elemento actual del array en la consola
+// ELEMENTOS DEL DOM
+
+const editButton = document.querySelector(".profile__edit-button");
+const editPopup = document.querySelector("#edit-popup");
+const closeButton = editPopup.querySelector(".popup__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const popupInputName = document.querySelector(".popup__input_type_name");
+const popupInputDescription = document.querySelector(
+  ".popup__input_type_description",
+);
+const profileForm = editPopup.querySelector(".popup__form");
+
+// FUNCIONES GENERALES
+
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+function fillProfileForm() {
+  popupInputName.value = profileTitle.textContent;
+  popupInputDescription.value = profileDescription.textContent;
+}
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(editPopup);
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = popupInputName.value;
+  profileDescription.textContent = popupInputDescription.value;
+  closeModal(editPopup);
+}
+
+// EVENTOS
+
+editButton.addEventListener("click", () => handleOpenEditModal());
+closeButton.addEventListener("click", () => closeModal(editPopup));
+profileForm.addEventListener("submit", handleProfileFormSubmit);
+
 initialCards.forEach(function (card) {
   console.log(card.name);
 });
